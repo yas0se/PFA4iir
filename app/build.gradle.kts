ec32ba1a55e9e778d79a8632ed519c257a3ec522
yas0se <128167310+yas0se@users.noreply.github.com>
2025-05-15 09:47:48 +0100

    start chat bot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,5 @@ dependencies {
     annotationProcessor(libs.glide.compiler) // Required for Glide
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.google.android.gms:play-services-maps:19.2.0")
-
+    implementation("org.json:json:20250107")
 }
---
+++ app/src/main/java/com/example/pfa4iir/assistant_virtuel.java
@@ -0,0 +1,24 @@
+package com.example.pfa4iir;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class assistant_virtuel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_assistant_virtuel);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
